d786051f756aff42f09db94f105c81cb0176e81e
Keith-koki77 <kokikeith@gmail.com>
2023-09-08 07:05:22 +0000

    file updated

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+Distributes web server
+"""
+from fabric.api import *
+import tarfile
+import os.path
+import re
+from datetime import datetime
+
+env.user = 'ubuntu'
+env.hosts = ["100.26.210.32", "100.26.157.149"]
+env.key_filename = "~/id_rsa"
+
+def do_pack():
+    """Distributes an archive to web servers
+    """
+    target = local("mkdir -p ./versions")
+    name = str(datetime.now()).replace(" ", '')
+    opt = re.sub(r'[^\w\s]', '', name)
+    tar = local('tar -cvzf versions/web_static_{}.tgz web_static'.format(opt))
+    if os.path.exists("./versions/web_static_{}.tgz".format(opt)):
+        return os.path.normpath("./versions/web_static_{}.tgz".format(opt))
+    else:
+        return None
+
+def do_deploy(archive_path):
+    """Distributes an archive to the web servers
+    """
+    if os.path.exists(archive_path) is False:
+        return False
+    try:
+        arc = archive_path.split("/")
+        base = arc[1].strip('.tgz')
+        put(archive_path, '/tmp/')
+        sudo('mkdir -p /data/web_static/releases/{}'.format(base))
+        main = "/data/web_static/releases/{}".format(base)
+        sudo('tar -xzf /tmp/{} -C {}/'.format(arc[1], main))
+        sudo('rm /tmp/{}'.format(arc[1]))
+        sudo('mv {}/web_static/* {}/'.format(main, main))
+        sudo('rm -rf /data/web_static/current')
+        sudo('ln -s {}/ "/data/web_static/current"'.format(main))
+        return True
+    except:
+        return False
+
+def deploy():
+    """Distributes an archive to web servers"""
+    path = do_pack()
+    if path is None:
+        return False
+    f = do_deploy(path)
+    return f
